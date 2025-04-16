bd269d672a265fbaab5bb86be1c5cd6a15b86f9b
Jack Weilage <jack@weilage.dev>
2025-04-15 21:57:47 -0700

    chore(archiver): Use `collect_from_cursor`, cleanup

--- apps/plustwo-archiver/src/main.rs
+++ apps/plustwo-archiver/src/main.rs
@@ -6,28 +6,29 @@ use plustwo_database::{
     DatabaseClient,
     entities::{self, sea_orm_active_enums::MessageKind},
 };
-use plustwo_twitch_gql::{CommentsByVideoAndCursorMessage, TwitchGqlClient};
+use plustwo_twitch_gql::{
+    CommentsByVideoAndCursorComment, CommentsByVideoAndCursorMessage, TwitchGqlClient,
+    collect_from_cursor, shared::video::TwitchVideo,
+};
 
 macro_rules! env_var {
     ($name:expr) => {
         ::std::env::var($name)
             .wrap_err_with(|| format!("Failed to find environment variable {}", $name))?
-            .as_str()
     };
 }
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     let client = TwitchGqlClient::new();
-    let db = DatabaseClient::new(env_var!("DATABASE_URL")).await?;
+    let db = DatabaseClient::new(&env_var!("DATABASE_URL")).await?;
 
-    let broadcaster = client
-        .get_stream_by_user(env_var!("TWITCH_BROADCASTER"))
-        .await?;
+    let broadcaster_name = env_var!("TWITCH_BROADCASTER");
+    let broadcaster = client.get_stream_by_user(&broadcaster_name).await?;
 
     db.insert_broadcaster(
         broadcaster.id.parse()?,
-        env_var!("TWITCH_BROADCASTER"),
+        &broadcaster_name,
         &broadcaster.profile_image_url,
     )
     .await?;
@@ -38,110 +39,95 @@ async fn main() -> eyre::Result<()> {
         "[{elapsed_precise}] {wide_bar} {pos}/{len} videos ({eta})",
     )?);
 
-    let mut video_cursor = None;
-    loop {
-        let videos = client
-            .get_videos_by_user_and_cursor(env_var!("TWITCH_BROADCASTER"), video_cursor.clone())
-            .await?;
+    let videos: Vec<TwitchVideo> = collect_from_cursor(async |cursor, _total, _videos| {
+        client
+            .get_videos_by_user_and_cursor(&broadcaster_name, cursor)
+            .await
+    })
+    .await?;
+    video_bar.set_length(videos.len() as u64);
 
-        video_bar.set_length(videos.total_count as u64);
-
-        for video in videos.edges {
-            video_cursor = video.cursor;
-
-            if currently_live_video
-                .as_ref()
-                .is_some_and(|id| id == &video.node.id)
-            {
-                video_bar.set_length(video_bar.length().unwrap() - 1);
-
-                continue;
-            }
-
-            if db
-                .get_broadcast(video.node.id.parse()?)
+    for video in videos {
+        // If the video is currently live or the broadcast has ended, skip it.
+        if currently_live_video
+            .as_ref()
+            .is_some_and(|id| id == &video.id)
+            || db
+                .get_broadcast(video.id.parse()?)
                 .await?
                 .is_some_and(|b| b.ended_at.is_some())
-            {
-                video_bar.set_length(video_bar.length().unwrap() - 1);
-                continue;
-            }
-
-            db.start_broadcast(
-                video.node.id.parse()?,
-                broadcaster.id.parse()?,
-                video.node.title,
-                video.node.created_at.naive_utc(),
-            )
-            .await?;
-
-            let comment_bar = ProgressBar::new(video.node.length_seconds).with_style(
-                ProgressStyle::with_template(
-                    "[{elapsed_precise}] {wide_bar} {pos}/{len} seconds ({eta})",
-                )?,
-            );
-
-            let mut chatters = HashMap::new();
-            let mut messages = Vec::new();
-
-            let mut comment_cursor = None;
-            loop {
-                let comments = client
-                    .get_comments_by_video_and_cursor(&video.node.id, comment_cursor.clone())
-                    .await?;
-
-                for comment in comments.edges {
-                    comment_cursor = comment.cursor;
-
-                    let Some(user) = comment.node.commenter else {
-                        continue;
-                    };
-
-                    comment_bar.set_position(
-                        (comment.node.created_at - video.node.created_at).num_seconds() as u64,
-                    );
-
-                    let Some(message_kind) = kind_from_message(&comment.node.message) else {
-                        continue;
-                    };
-
-                    let chatter = entities::chatters::Model {
-                        id: user.id.parse()?,
-                        display_name: user.display_name,
-                    };
-
-                    chatters.insert(chatter.id, chatter.clone());
-                    messages.push(entities::messages::Model {
-                        id: comment.node.id,
-                        broadcast_id: video.node.id.parse()?,
-                        chatter_id: chatter.id,
-                        sent_at: comment.node.created_at.naive_utc(),
-                        message_kind,
-                    });
-                }
-
-                if !comments.page_info.has_next_page {
-                    break;
-                }
-            }
-
-            db.insert_many_chatters(chatters.values()).await?;
-            db.insert_many_messages(&messages).await?;
-
-            db.end_broadcast(
-                broadcaster.id.parse()?,
-                (video.node.created_at + Duration::from_secs(video.node.length_seconds))
-                    .naive_utc(),
-            )
-            .await?;
-
-            comment_bar.finish();
+        {
             video_bar.inc(1);
+            continue;
         }
 
-        if !videos.page_info.has_next_page {
-            break;
+        db.start_broadcast(
+            video.id.parse()?,
+            broadcaster.id.parse()?,
+            video.title.clone(),
+            video.created_at.naive_utc(),
+        )
+        .await?;
+
+        let comment_bar =
+            ProgressBar::new(video.length_seconds).with_style(ProgressStyle::with_template(
+                "[{elapsed_precise}] {msg} {wide_bar} {pos}/{len} seconds ({eta})",
+            )?);
+        comment_bar.set_message(video.title);
+
+        let mut chatters = HashMap::new();
+        let mut messages = Vec::new();
+
+        for comment in collect_from_cursor(
+            async |cursor, _length, comments: &Vec<CommentsByVideoAndCursorComment>| {
+                if let Some(comment) = comments.last() {
+                    comment_bar
+                        .set_position((comment.created_at - video.created_at).num_seconds() as u64);
+                }
+
+                client
+                    .get_comments_by_video_and_cursor(&video.id, cursor)
+                    .await
+            },
+        )
+        .await?
+        {
+            // If the user cannot be parsed, skip the comment.
+            let Some(user) = comment.commenter else {
+                continue;
+            };
+            // If the comment is not a +2 or -2, skip it.
+            let Some(message_kind) = kind_from_message(&comment.message) else {
+                continue;
+            };
+
+            let chatter = entities::chatters::Model {
+                id: user.id.parse()?,
+                display_name: user.display_name,
+            };
+
+            chatters.insert(chatter.id, chatter.clone());
+            messages.push(entities::messages::Model {
+                id: comment.id,
+                broadcast_id: video.id.parse()?,
+                chatter_id: chatter.id,
+                sent_at: comment.created_at.naive_utc(),
+                message_kind,
+            });
         }
+
+        db.insert_many_chatters(chatters.values()).await?;
+        db.insert_many_messages(&messages).await?;
+
+        db.end_broadcast(
+            broadcaster.id.parse()?,
+            (video.created_at + Duration::from_secs(video.length_seconds)).naive_utc(),
+            Some(video.id.parse()?),
+        )
+        .await?;
+
+        comment_bar.finish();
+        video_bar.inc(1);
     }
 
     Ok(())
